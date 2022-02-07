223f778dc5cb038c1dd689eca851e91aa394e64b
Kato Shinya <kato.shinya.dev@gmail.com>
2022-02-07 13:10:54 +0900

    Optimize example according to analysis rules.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,21 +4,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:painter/painter.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return const MaterialApp(
       title: 'Painter Example',
-      home: new ExamplePage(),
+      home: ExamplePage(),
     );
   }
 }
 
 class ExamplePage extends StatefulWidget {
+  const ExamplePage({Key? key}) : super(key: key);
+
   @override
-  _ExamplePageState createState() => new _ExamplePageState();
+  _ExamplePageState createState() => _ExamplePageState();
 }
 
 class _ExamplePageState extends State<ExamplePage> {
@@ -31,7 +35,7 @@ class _ExamplePageState extends State<ExamplePage> {
   }
 
   static PainterController _newController() {
-    PainterController controller = new PainterController();
+    PainterController controller = PainterController();
     controller.thickness = 5.0;
     controller.backgroundColor = Colors.green;
     return controller;
@@ -42,8 +46,8 @@ class _ExamplePageState extends State<ExamplePage> {
     List<Widget> actions;
     if (_finished) {
       actions = <Widget>[
-        new IconButton(
-          icon: new Icon(Icons.content_copy),
+        IconButton(
+          icon: const Icon(Icons.content_copy),
           tooltip: 'New Painting',
           onPressed: () => setState(() {
             _finished = false;
@@ -53,41 +57,48 @@ class _ExamplePageState extends State<ExamplePage> {
       ];
     } else {
       actions = <Widget>[
-        new IconButton(
-            icon: new Icon(
-              Icons.undo,
-            ),
-            tooltip: 'Undo',
-            onPressed: () {
-              if (_controller.isEmpty) {
-                showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) =>
-                        new Text('Nothing to undo'));
-              } else {
-                _controller.undo();
-              }
-            }),
-        new IconButton(
-            icon: new Icon(Icons.delete),
+        IconButton(
+          icon: const Icon(
+            Icons.undo,
+          ),
+          tooltip: 'Undo',
+          onPressed: () {
+            if (_controller.isEmpty) {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) =>
+                      const Text('Nothing to undo'));
+            } else {
+              _controller.undo();
+            }
+          },
+        ),
+        IconButton(
+            icon: const Icon(Icons.delete),
             tooltip: 'Clear',
             onPressed: _controller.clear),
-        new IconButton(
-            icon: new Icon(Icons.check),
-            onPressed: () => _show(_controller.finish(), context)),
+        IconButton(
+          icon: const Icon(Icons.check),
+          onPressed: () => _show(_controller.finish(), context),
+        ),
       ];
     }
-    return new Scaffold(
-      appBar: new AppBar(
-          title: const Text('Painter Example'),
-          actions: actions,
-          bottom: new PreferredSize(
-            child: new DrawBar(_controller),
-            preferredSize: new Size(MediaQuery.of(context).size.width, 30.0),
-          )),
-      body: new Center(
-          child: new AspectRatio(
-              aspectRatio: 1.0, child: new Painter(_controller))),
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Painter Example'),
+        actions: actions,
+        bottom: PreferredSize(
+          child: DrawBar(controller: _controller),
+          preferredSize: Size(MediaQuery.of(context).size.width, 30.0),
+        ),
+      ),
+      body: Center(
+        child: AspectRatio(
+          aspectRatio: 1.0,
+          child: Painter(_controller),
+        ),
+      ),
     );
   }
 
@@ -95,124 +106,146 @@ class _ExamplePageState extends State<ExamplePage> {
     setState(() {
       _finished = true;
     });
-    Navigator.of(context)
-        .push(new MaterialPageRoute(builder: (BuildContext context) {
-      return new Scaffold(
-        appBar: new AppBar(
-          title: const Text('View your image'),
-        ),
-        body: new Container(
-            alignment: Alignment.center,
-            child: new FutureBuilder<Uint8List>(
-              future: picture.toPNG(),
-              builder:
-                  (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.done:
-                    if (snapshot.hasError) {
-                      return new Text('Error: ${snapshot.error}');
-                    } else {
-                      return Image.memory(snapshot.data!);
-                    }
-                  default:
-                    return new Container(
-                        child: new FractionallySizedBox(
-                      widthFactor: 0.1,
-                      child: new AspectRatio(
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('View your image'),
+            ),
+            body: Container(
+              alignment: Alignment.center,
+              child: FutureBuilder<Uint8List>(
+                future: picture.toPNG(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.done:
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else {
+                        return Image.memory(snapshot.data!);
+                      }
+                    default:
+                      return const FractionallySizedBox(
+                        widthFactor: 0.1,
+                        child: AspectRatio(
                           aspectRatio: 1.0,
-                          child: new CircularProgressIndicator()),
-                      alignment: Alignment.center,
-                    ));
-                }
-              },
-            )),
-      );
-    }));
+                          child: CircularProgressIndicator(),
+                        ),
+                        alignment: Alignment.center,
+                      );
+                  }
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
 
 class DrawBar extends StatelessWidget {
-  final PainterController _controller;
+  const DrawBar({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
 
-  DrawBar(this._controller);
+  final PainterController controller;
 
   @override
   Widget build(BuildContext context) {
-    return new Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        new Flexible(child: new StatefulBuilder(
+        Flexible(
+          child: StatefulBuilder(
             builder: (BuildContext context, StateSetter setState) {
-          return new Container(
-              child: new Slider(
-            value: _controller.thickness,
-            onChanged: (double value) => setState(() {
-              _controller.thickness = value;
-            }),
-            min: 1.0,
-            max: 20.0,
-            activeColor: Colors.white,
-          ));
-        })),
-        new StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-          return new RotatedBox(
-              quarterTurns: _controller.eraseMode ? 2 : 0,
+              return Slider(
+                value: controller.thickness,
+                onChanged: (double value) => setState(() {
+                  controller.thickness = value;
+                }),
+                min: 1.0,
+                max: 20.0,
+                activeColor: Colors.white,
+              );
+            },
+          ),
+        ),
+        StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return RotatedBox(
+              quarterTurns: controller.eraseMode ? 2 : 0,
               child: IconButton(
-                  icon: new Icon(Icons.create),
-                  tooltip: (_controller.eraseMode ? 'Disable' : 'Enable') +
-                      ' eraser',
-                  onPressed: () {
-                    setState(() {
-                      _controller.eraseMode = !_controller.eraseMode;
-                    });
-                  }));
-        }),
-        new ColorPickerButton(_controller, false),
-        new ColorPickerButton(_controller, true),
+                icon: const Icon(Icons.create),
+                tooltip:
+                    (controller.eraseMode ? 'Disable' : 'Enable') + ' eraser',
+                onPressed: () {
+                  setState(() {
+                    controller.eraseMode = !controller.eraseMode;
+                  });
+                },
+              ),
+            );
+          },
+        ),
+        ColorPickerButton(controller: controller, background: false),
+        ColorPickerButton(controller: controller, background: true),
       ],
     );
   }
 }
 
 class ColorPickerButton extends StatefulWidget {
-  final PainterController _controller;
-  final bool _background;
+  const ColorPickerButton({
+    Key? key,
+    required this.controller,
+    required this.background,
+  }) : super(key: key);
 
-  ColorPickerButton(this._controller, this._background);
+  final PainterController controller;
+  final bool background;
 
   @override
-  _ColorPickerButtonState createState() => new _ColorPickerButtonState();
+  _ColorPickerButtonState createState() => _ColorPickerButtonState();
 }
 
 class _ColorPickerButtonState extends State<ColorPickerButton> {
   @override
   Widget build(BuildContext context) {
-    return new IconButton(
-        icon: new Icon(_iconData, color: _color),
-        tooltip: widget._background
-            ? 'Change background color'
-            : 'Change draw color',
-        onPressed: _pickColor);
+    return IconButton(
+      icon: Icon(_iconData, color: _color),
+      tooltip:
+          widget.background ? 'Change background color' : 'Change draw color',
+      onPressed: _pickColor,
+    );
   }
 
   void _pickColor() {
     Color pickerColor = _color;
     Navigator.of(context)
-        .push(new MaterialPageRoute(
-            fullscreenDialog: true,
-            builder: (BuildContext context) {
-              return new Scaffold(
-                  appBar: new AppBar(
-                    title: const Text('Pick color'),
-                  ),
-                  body: new Container(
-                      alignment: Alignment.center,
-                      child: new ColorPicker(
-                        pickerColor: pickerColor,
-                        onColorChanged: (Color c) => pickerColor = c,
-                      )));
-            }))
+        .push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Pick color'),
+            ),
+            body: Container(
+              alignment: Alignment.center,
+              child: ColorPicker(
+                pickerColor: pickerColor,
+                onColorChanged: (Color c) => pickerColor = c,
+              ),
+            ),
+          );
+        },
+      ),
+    )
         .then((_) {
       setState(() {
         _color = pickerColor;
@@ -220,18 +253,18 @@ class _ColorPickerButtonState extends State<ColorPickerButton> {
     });
   }
 
-  Color get _color => widget._background
-      ? widget._controller.backgroundColor
-      : widget._controller.drawColor;
+  Color get _color => widget.background
+      ? widget.controller.backgroundColor
+      : widget.controller.drawColor;
 
   IconData get _iconData =>
-      widget._background ? Icons.format_color_fill : Icons.brush;
+      widget.background ? Icons.format_color_fill : Icons.brush;
 
   set _color(Color color) {
-    if (widget._background) {
-      widget._controller.backgroundColor = color;
+    if (widget.background) {
+      widget.controller.backgroundColor = color;
     } else {
-      widget._controller.drawColor = color;
+      widget.controller.drawColor = color;
     }
   }
 }
